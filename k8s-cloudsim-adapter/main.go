95252fec94f29f14e1d27fd79a4851e6b6ba557c
Florin-Vladimir Stancu <sc20fvs@leeds.ac.uk>
2025-06-16 13:29:31 +0100

    Define a "Communicator" class.

--- k8s-cloudsim-adapter/main.go
+++ k8s-cloudsim-adapter/main.go
@@ -7,16 +7,17 @@ import (
 
 // main function is the entry point of the Go control plane application.
 func main() {
-	port := ":8080"                       // Port on which the control plane HTTP server will listen
-	extenderURL = "http://localhost:8081" // URL of the mock scheduler extender
+	port := ":8080"                        // Port on which the control plane HTTP server will listen
+	extenderURL := "http://localhost:8081" // URL of the mock scheduler extender
 
 	log.Printf("Starting K8s Sim Control Plane POC on port %s\n", port)
 	log.Printf("Configured to call scheduler extender at %s\n", extenderURL)
 
 	// Register HTTP handlers for different API endpoints.
-	http.HandleFunc("/schedule-pods", handleBatchPods)
-	http.HandleFunc("/nodes", handleNodes)
-	http.HandleFunc("/pods/", handlePodStatus) // Handles requests like /pods/{id}/status
+	comm := NewCommunicator("http://localhost:8081")
+	http.HandleFunc("/nodes", comm.HandleNodes)
+	http.HandleFunc("/schedule-pods", comm.HandleBatchPods)
+	http.HandleFunc("/pods/", comm.HandlePodStatus)
 
 	// Start the HTTP server and log any fatal errors (e.g., port already in use).
 	log.Fatal(http.ListenAndServe(port, nil))
