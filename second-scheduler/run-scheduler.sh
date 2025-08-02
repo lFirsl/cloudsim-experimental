a2f286a3d8e26e5e669a79b53238d6d948cf87d5
Florin-Vladimir Stancu <sc20fvs@leeds.ac.uk>
2025-08-02 14:08:59 +0100

    Add scripts and docker files to run the second-scheduler easier.

---
+++ second-scheduler/run-scheduler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker run --rm \
+  --network host \
+  -v $(pwd)/my-scheduler.yaml:/etc/kube-scheduler/my-scheduler.yaml \
+  -v $(pwd)/kubeconfig.yaml:/etc/kube-scheduler/kubeconfig.yaml \
+  -v $(pwd)/admin.crt:/etc/kube-scheduler/admin.crt \
+  -v $(pwd)/admin.key:/etc/kube-scheduler/admin.key \
+  -v $(pwd)/ca.crt:/etc/kube-scheduler/ca.crt \
+  registry.k8s.io/kube-scheduler:v1.33.0 \
+  kube-scheduler \
+    --config=/etc/kube-scheduler/my-scheduler.yaml \
+    --secure-port=10260 \
+    --v=3
+
+
+# Run this with
+# chmod +x run-scheduler.sh
+# ./run-scheduler.sh &
